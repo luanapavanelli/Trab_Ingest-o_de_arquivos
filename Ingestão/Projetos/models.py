d31d16aa2ddb3d6b19b9a6fe2a28d00f9c686623
ArturSiqueiraSudre <artur.sudre@gmail.com>
2026-04-12 15:44:58 -0300

    Commit 1: Estruturação Inicial Banco de Dados
    Adicionando os modelos de Projetos e Arquivos ao Banco de Dados

--- Ingestão/Projetos/models.py
+++ Ingestão/Projetos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Projeto(models.Model):
+    nome = models.CharField(max_length=100)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    descricao = models.TextField(blank=True, null=True, verbose_name="Descrição")
+    ultima_alteracao = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.nome
+
+class Arquivo(models.Model):
+    # A mágica do relacionamento acontece AQUI, na mesma página:
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE, related_name='arquivos')
+    documento = models.FileField(upload_to='arquivos/')
+    nome_original = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=20) 
+    data_ingestao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome_original
