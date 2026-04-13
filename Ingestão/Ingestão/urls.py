8e70a263b7fd03c905ee2f95c5228387839eca6e
LUANA_PAVANELLI <lurpavanelli@gmail.com>
2026-04-12 21:02:03 -0300

    Adicionado HTML inicial do dashboard e configurado ambiente base

--- Ingestão/Ingestão/urls.py
+++ Ingestão/Ingestão/urls.py
@@ -1,24 +1,15 @@
-"""
-URL configuration for Ingestão project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from Projetos import views  # Isso conecta o seu app de projetos!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Esta é a linha que estava faltando. Ela diz que a URL raiz ('') vai carregar o seu HTML:
+    path('', views.dashboard, name='dashboard'), 
+]
+
+# Isso permite que os PDFs e arquivos abram no navegador depois do upload
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
