9c20680135dd2fcd0d9aa3c2b6b45d3132ba5148
ArturSiqueiraSudre <artur.sudre@gmail.com>
2026-05-17 13:18:36 -0300

    Micro Ajuste front end:
    Escolha "inteligente" de projeto

--- Nova_Arquitetura_Ingestao/Frontend/src/app/App.tsx
+++ Nova_Arquitetura_Ingestao/Frontend/src/app/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from './components/ThemeProvider';
+import ThemeToggle from './components/ThemeToggle';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Toaster position="top-right" richColors />
-      <Routes>
-        <Route path="/" element={<Projects />} />
-        <Route path="/dashboard/:projetoId" element={<Dashboard />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Toaster position="top-right" richColors theme="dark" />
+        <ThemeToggle />
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/dashboard/:projetoId" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
